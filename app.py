20d8293c9e101779af570e482c93613c38f6185c
abdullahsumbal <abdullahsumbal@gmail.com>
2018-12-22 10:55:29 -0500

    fdsf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, make_response
 from helper import *
 app = Flask(__name__)
+app.secret_key = 'hamzaelahgdfgsdfdfgisquash'
 
 @app.route('/')
 def index():
