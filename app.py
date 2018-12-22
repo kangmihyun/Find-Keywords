e1358fa6c7d71ebc178748c117c3f334722c72f2
abdullahsumbal <abdullahsumbal@gmail.com>
2018-12-22 10:45:17 -0500

    i think i am done

--- app.py
+++ app.py
@@ -6,21 +6,32 @@ app = Flask(__name__)
 def index():
     return redirect(url_for('home'))
 
-@app.route('/home')
+@app.route('/home', methods = ['POST', 'GET'])
 def home():
-    url = "http://www.is.mcgill.ca/studentaid/workstudy/postings/index.htm"
-    locNet = urlparse(url).netloc
-    visited = []
-    current_level = 0
-    level_limit =  4
-    sameDomain = True
-    keywords = ["python"]
-    keywords_pattern = re.compile('|'.join(keywords))
-    DFS(url, visited, current_level, level_limit, locNet)
-    # print(visited)
-    links = filter(visited, keywords_pattern)
-    print(links)
-    return render_template('search.html', links=links, num=len(links))
+    if('WebsiteURL' not in request.form or 'Keywords' not in request.form):
+        links = {}
+        return render_template('search.html', links=links, num=len(links))
+    else:
+
+        url = request.form['WebsiteURL']
+        keywords = request.form['Keywords'].split()
+        if 'Restrict' in request.form:
+            locNet = urlparse(url).netloc
+            sameDomain = True
+        else:
+            locNet = ""
+            sameDomain = False
+
+        visited = []
+        current_level = 0
+        level_limit = request.form['SelectLevel']
+
+        keywords_pattern = re.compile('|'.join(keywords))
+        DFS(url, visited, current_level, level_limit, locNet, sameDomain)
+        # print(visited)
+        links = filter(visited, keywords_pattern)
+        print(links)
+        return render_template('search.html', links=links, num=len(links))
 
 if __name__ == '__main__':
    app.run(debug = True)
